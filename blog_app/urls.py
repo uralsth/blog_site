79b6d03bb64f706741110c25feb7c10b8e40c15b
uralsth <shresthaural100@gmail.com>
2022-07-15 16:24:02 +0545

    url patter for sharing post

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
