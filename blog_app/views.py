1f046a22a1d485863be484e145d543b7e23d4474
uralsth <shresthaural100@gmail.com>
2022-07-16 21:52:16 +0545

    views for comment features

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import CommentForm, EmailPostForm
 from django.conf import settings
 from django.core.mail import send_mail
 
@@ -23,7 +23,20 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog_app/post/detail.html', {'post': post})
+    # List of active comment for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog_app/post/detail.html', {'post': post, 'comments':comments, 'new_comment': new_comment, 'comment_form':comment_form})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
