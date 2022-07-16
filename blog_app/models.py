1d0ac239db22ab4abbd63d8feb4bd38e2628576a
uralsth <shresthaural100@gmail.com>
2022-07-16 21:49:57 +0545

    created comment model

--- blog_app/models.py
+++ blog_app/models.py
@@ -7,6 +7,7 @@ class PublishedManger(models.Manager):
     def get_queryset(self):
         return super(PublishedManger, self).get_queryset().filter(status='published')
 
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -34,3 +35,19 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created', )
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
