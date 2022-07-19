32c6e0c727e6deb9550a46ea456a9af5369b5c3d
uralsth <shresthaural100@gmail.com>
2022-07-19 19:30:17 +0545

    urlpatterns for sitemap

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from blog_app.sitemaps import PostSitemap
+
+sitemaps = {
+    'post': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog_app.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
