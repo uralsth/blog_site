f35d5ffb1ab4a84299c3a855d762185528112819
uralsth <shresthaural100@gmail.com>
2022-07-19 19:29:41 +0545

    views to retrieve similar posts

--- blog_app/views.py
+++ blog_app/views.py
@@ -5,6 +5,7 @@ from .forms import CommentForm, EmailPostForm
 from django.conf import settings
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -41,7 +42,11 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request, 'blog_app/post/detail.html', {'post': post, 'comments':comments, 'new_comment': new_comment, 'comment_form':comment_form})
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog_app/post/detail.html', {'post': post, 'comments':comments, 'new_comment': new_comment, 'comment_form':comment_form, 'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
