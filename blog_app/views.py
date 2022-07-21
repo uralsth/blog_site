6897940ab5c2e10fba3000209594dd8bfbb21f1a
uralsth <shresthaural100@gmail.com>
2022-07-21 18:36:01 +0545

    added different method to search query

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Post, Comment
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from django.conf import settings
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery, TrigramSimilarity
 
 
 def post_list(request, tag_slug=None):
@@ -64,3 +65,21 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog_app/post/share.html', {'post':post, 'form': form, 'sent':sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            # results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            # search_vector = SearchVector('title', 'body')
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            # results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
+            # results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query)).filter(similarity=0.1).order_by('-similarity')
+    return render(request, 'blog_app/post/search.html', {'form': form, 'query': query, 'results': results})
