68f7750edbd789f79c858b060eaf85e60dc38aa5
uralsth <shresthaural100@gmail.com>
2022-07-21 18:35:43 +0545

    integrated postgres database

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
     'blog_app',
     'taggit',
+
 
 ]
 
@@ -83,8 +85,10 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD')
     }
 }
 
